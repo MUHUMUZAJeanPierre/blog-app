fc2db1a715396b4b9d0c5048f7ff592a99e47d40
MUHUMUZAJeanPierre <muhumuzajeanpierre9@gmail.com>
2025-10-01 15:17:32 +0200

    feat

--- server.js
+++ server.js
@@ -6,17 +6,15 @@ import postRoutes from "./routes/postRoutes.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
-// Connect to DB
 connectDB();
 
-// Routes
+
 app.use("/api/posts", postRoutes);
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
