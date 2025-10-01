4ad3382860ec040b3fa4f24701d3091333feec41
MUHUMUZAJeanPierre <muhumuzajeanpierre9@gmail.com>
2025-10-01 16:05:57 +0200

    feat

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import postRoutes from "./routes/postRoutes.js";
+import mongoose from "mongoose";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,7 +11,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-connectDB();
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error("MongoDB Connection Error:", err.message));
+
 
 
 app.use("/api/posts", postRoutes);
