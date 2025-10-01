71e0581bb7b7b964752e923ae48dd8fa7caf3b12
MUHUMUZAJeanPierre <muhumuzajeanpierre9@gmail.com>
2025-10-01 15:50:17 +0200

    feat

--- config/db.js
+++ config/db.js
@@ -1,14 +1,17 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 dotenv.config();
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGO_URI);
-    console.log("MongoDB Connected");
+    const mongoURI ="mongodb+srv://jmuhumuza_db_user:9lwptbfnm7kUQtof@cluster0.a9eswmd.mongodb.net/blogDB?retryWrites=true&w=majority";
+
+    await mongoose.connect(mongoURI);
+    console.log("✅ MongoDB Connected");
   } catch (error) {
     console.error("MongoDB Connection Error:", error.message);
-    process.exit(1);
+    process.exit(1); 
   }
 };
 
